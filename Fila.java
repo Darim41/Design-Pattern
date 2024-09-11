d609f5c8153cde46ec5b133356673df1623a51a3
Darim41 <fabio.darim.zulmira@gmail.com>
2024-09-10 21:05:08 -0300

    Add files via upload

---
+++ Fila.java
@@ -0,0 +1,20 @@
+public class Fila {
+
+    private static Fila instance;
+    
+    private static void Fila(){}
+
+    private static void ImprimeDocumento(){}
+
+    private static void RemoveDocumento(){}
+
+    private static void RemoveTodosDocumentos(){}
+
+    public static Fila getInstance() {
+        if (instance == null) {
+            instance = new Fila();
+        }
+        return instance;
+    }
+    
+}
